e07a3f312da31ccee77d731ac25f60a496145e9b
Edekje <ethanvanwoerkom@yahoo.com>
2015-02-06 17:34:33 +0100

    Correcte Euler 5

--- Euler_5.py
+++ Euler_5.py
@@ -1,30 +1,74 @@
-# Smart solution: bereken de kgv van 1-10 samen
+"""
+Uitleg vooraf van het algoritme:
+Bereken de kgv van 1 en 2, dan van dat kgv en 3 etc. tm 20
+
+Om de kgv te berekenen: neem de pfo van elk, tel die bijelkaar op als lijsten
+en verwijder de doorsnede van beiden eenmaal (doorsnede=gemeenschappelijke)
+
+Berekening doorsnede lijst1, lijst2:
+Voor elk element in lijst1:
+als het in lijst2 zit voeg het bij de te returnen waardes en verwijder het uit 2
+
+Pfo is simpel: die zoekt tot de wortel
+"""
+
 import math
-def get_factors(num):
+
+def doorsnede(list1, list2):
+    # Creeer de lijst van het antwoord. Het is nodig nieuwe objecten te maken met
+    # list() omdat we anders de waardes van list1 in het hoofdprogramma veranderen!
+    returnme = []
+    lijst1 = list(list1)
+    lijst2 = list(list2)
+    
+    for i in lijst1:
+        if i in lijst2:
+            returnme.append(i)
+            lijst2.remove(i)
+    
+    return returnme
+
+def pfo(num):
     factors = []
+
     while num != 1:
-        max = 1 + int(math.sqrt(num))
-        for i in range(2,max):
-            if(num % i == 0):
+        # Zoeken verder dan de wortel is nutteloos: dan is het priem
+        maxi = int(math.sqrt(num))+1
+
+        for i in range(2, maxi):
+            if num%i == 0:
                 factors.append(i)
-                num = int(num/i)
+                num /= i
+                num = int(num)
                 break
         else:
+            # Een loop entert een else erna als er geen breaks waren
+            # In dit geval is het dan dus een priem
             factors.append(num)
-            break
+            num = 1
+
     return factors
 
-def kgv(list1, list2):
-    list3 = list1
-    for i in list1:
-        print(i)
-        try:
-            list2.remove(i)
-        except:
-            list3.append(i)
-    return list3
 
-first = get_factors(27)
-second = get_factors(33)
+def kgv(num1, num2):
+    list1 = pfo(num1)
+    list2 = pfo(num2)
+    list3 = doorsnede(list1, list2)
+    list4 = list1+list2
+    # Verwijder de dubbele elementen in 3 dmv de doorsnede
+    for i in list3:
+        list4.remove(i)
 
-print(kgv(first,second))
+    num = 1
+    # Vind wat ze vermenigvuldigd met elkaar zijn
+    for i in list4:
+        num *= i
+
+    return num
+
+num = 1
+
+# Pak de kgvs van 1-20 samen
+for i in range(2, 21):
+    num = kgv(num, i)
+print(num)
